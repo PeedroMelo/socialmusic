a1383e1e145eda2c507cbada7e95cdbb7143b873
Pedro <vieirapcm@gmail.com>
2021-03-02 00:57:50 -0300

    Adding environments variables

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -6,6 +6,7 @@
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
  */
+require('dotenv').config(); // Requiring environments variables
 
 var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
